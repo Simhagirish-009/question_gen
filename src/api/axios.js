18ffd8065ee197a781a34c4ffa5032539c7d17cb
Simha_girish <simhagirishbotta@gmail.com>
2025-11-13 15:12:23 +0530

    api logic magic

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://question-gen-v3cj.onrender.com",
+  baseURL: "https://question-gen-v3cj.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
